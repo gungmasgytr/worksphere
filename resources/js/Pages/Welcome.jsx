288f39fd3eb90391c439ed3365aac9193ca3617e
gungmasgytr <anakagung0881@gmail.com>
2025-12-30 22:34:50 +0800

    Set Up projek menggunakan Laravel + React Inertia

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-blue-600">
+                Welcome React + Inertia + Tailwind
+            </h1>
+        </div>
+    )
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import './bootstrap'
+import '../css/app.css'
+
+import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/react'
+
+createInertiaApp({
+  resolve: name => import(`./Pages/${name}.jsx`),
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />)
+  },
+})
